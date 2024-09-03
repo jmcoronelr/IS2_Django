7cae1692ca04bc34feb845bf0f895fd11cec9f95
Marcelo Coronel <jmcoronelr19@gmail.com>
2024-09-03 16:17:22 -0400

    Paginas de Inicio con y sin autenticacion

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from . import views
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def home(request):
+    if request.user.is_authenticated: #redirecciona en relacion al estado de autenticacion
+        return redirect('sistema')
     return render(request, 'home.html')
 
 def sistema(request):
+    if not (request.user.is_authenticated): #redirecciona en relacion al estado de autenticacion
+        return redirect('home')
     return render(request, 'sistema.html')
