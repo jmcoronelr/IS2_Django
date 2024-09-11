01a00846b11ec49e5dd6186050384fbb5be2bff3
Jonathan-Alcaraz <jonathanalcaraz.e@fpuna.edu.py>
2024-09-11 14:52:07 -0400

    SSO arreglado y verificacion superuser

--- myproject/roles/templatetags/tiene_permiso.py
+++ myproject/roles/templatetags/tiene_permiso.py
@@ -1,10 +1,17 @@
 from django import template
 from roles.models import RolEnCategoria, Permiso
+from django.contrib.auth.models import AnonymousUser
 
 register = template.Library()
 
 @register.filter(name="tiene_permiso")
 def tiene_permiso(usuario, permiso_nombre):
+    # Verifica si el usuario es anónimo
+    if isinstance(usuario, AnonymousUser):
+        return False
+    # Verifica si el usuario es superuser
+    if usuario.is_superuser:
+        return True
     categorias = RolEnCategoria.objects.filter(usuario=usuario)
     permisos = Permiso.objects.none()
     for rol_categoria in categorias:
