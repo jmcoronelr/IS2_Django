bf00f92337b2a43c3caaa836a068d3369cd05cbd
Esteban Jara <ejara176@gmail.com>
2024-10-11 23:54:59 -0300

    listo

--- myproject/reportes/views.py
+++ myproject/reportes/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from content.models import Content
+from django.http import JsonResponse
 
 def reportes_home(request):
     return render(request, 'reportes/reportes_home.html')
+
+def reporte_likes_json(request):
+    # Obtener todas las publicaciones ordenadas por número de likes, de mayor a menor
+    contenidos = Content.objects.all().order_by('-likes')
+    data = []
+
+    # Construir la respuesta en formato JSON
+    for contenido in contenidos:
+        data.append({
+            'title': contenido.title,
+            'likes': contenido.likes,
+        })
+    
+    return JsonResponse(data, safe=False)
